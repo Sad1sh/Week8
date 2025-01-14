831bf4899e518ecf567869e7c36e3f42b8ff15cf
Sadish <rajsadish10@gmail.com>
2025-01-14 08:09:11 +0545

    Second Commit

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class GradeCalculator {
+    public static char calculateGrade(double marks) {
+        if (marks >= 90) {
+            return 'A';
+        } else if (marks >= 80) {
+            return 'B';
+        } else if (marks >= 70) {
+            return 'C';
+        } else if (marks >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
 }
